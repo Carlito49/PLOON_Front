23e13c00003deaf7303abb748b37b74aeff8f3cb
Carlito49 <charlesquellin@hotmail.com>
2022-02-03 11:16:52 +0100

    deuxieme changement

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Accueil from './components/Accueil';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />  
+      <Accueil />
     </View>
   );
 }
---
+++ components/Accueil.js
@@ -0,0 +1,11 @@
+import react from "react";
+import { View, Text } from 'react-native'
+
+export default function Accueil(props)
+{   
+    return(
+        <View>
+            
+        </View>
+    );
+}
