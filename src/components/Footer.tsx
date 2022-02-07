ff284c0b73216490e799cdb8d92f3d2d3cec3b65
Carlit49 <charlesquellin@hotmail.com>
2022-02-07 12:52:57 +0100

    Changement footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,22 +1,13 @@
 import React from "react";
 import MyButton from './MyButton'
-import { NavigationContainer } from '@react-navigation/native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { View } from "react-native";
 
 const Footer = () => {
     return(
         <View>
-            <NavigationContainer>
-                <Tab.Navigator>
-                    <Tab.Screen name="Home" component={MyButton}/>
-                </Tab.Navigator>
-            </NavigationContainer>
             <MyButton image="../images/home.png" />
         </View>
     );
 }
-
-const Tab = createBottomTabNavigator();
 
 export default Footer;
